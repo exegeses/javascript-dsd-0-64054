6402484f07bb1fb1fc5f48705f2e6d05e832cbc1
Marcos <exegeses@gmail.com>
2023-07-08 13:00:42 -0300

    Práctica clase 2

---
+++ clase2/js/condicionales.js
@@ -0,0 +1,17 @@
+//ubicamos elemento dentro del documento
+const contenedor = document.getElementById('contenedor');
+
+// hacer pregunta al usuario
+// almacenar la respuesta en memoria
+let respuesta = prompt('¿qué seleccionado ganóa la copa mundial Qatar 2023?');
+
+if( respuesta.toLowerCase() == 'argentina' ){
+    //bloque a ejecutar si la condición es true
+    //contenedor.innerText = 'Correcto';
+    contenedor.innerHTML = 'Correcto <br> <img src="imgs/ok.png">';
+}
+else{
+    //bloque a ejecutar si la condición es false
+    //contenedor.innerText = 'Incorrecto';
+    contenedor.innerHTML = 'Inorrecto <br> <img src="imgs/error.png">';
+}
